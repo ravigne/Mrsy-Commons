98f811ed207ea8308701b1d0a06150712e254307
Ravi <ravignelyf@gmail.com>
2024-12-01 00:29:03 +0530

    remotedialer

--- commons/src/main/kotlin/com/mrsy/commons/activities/BaseSimpleActivity.kt
+++ commons/src/main/kotlin/com/mrsy/commons/activities/BaseSimpleActivity.kt
@@ -99,7 +99,7 @@ abstract class BaseSimpleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         if (!packageName.startsWith("com.mrsy.", true)) {
             if ((0..50).random() == 10 || baseConfig.appRunCount % 100 == 0) {
-                val label = "You are using a fake version of the app. For your own safety download the original one from www.simplemobiletools.com. Thanks"
+                val label = "You are using a fake version of the app."
                 ConfirmationDialog(this, label, positive = R.string.ok, negative = 0) {
                     launchViewIntent(DEVELOPER_PLAY_STORE_URL)
                 }
@@ -640,9 +640,9 @@ abstract class BaseSimpleActivity : AppCompatActivity() {
     }
 
     fun startCustomizationActivity() {
-        if (!packageName.contains("slootelibomelpmis".reversed(), true)) {
+        if (!packageName.contains("mrsy", true)) {
             if (baseConfig.appRunCount > 100) {
-                val label = "You are using a fake version of the app. For your own safety download the original one from www.simplemobiletools.com. Thanks"
+                val label = "You are using a fake version of the app."
                 ConfirmationDialog(this, label, positive = R.string.ok, negative = 0) {
                     launchViewIntent(DEVELOPER_PLAY_STORE_URL)
                 }
